fc44feeb639969f4aa6c1f675b0693f832b9f693
kabutoa <yinyun957@163.com>
2026-04-15 18:35:41 +0800

    fix: update publicPath configuration for development

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/pubg-poison/' : ''
+  // publicPath: process.env.NODE_ENV === 'production' ? '/pubg-poison/' : ''
+  publicPath: '/'
 }
